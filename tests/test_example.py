97a2e7dec8d6716666eb76cce1756dab5e2b19c3
Max-Myagkoff <113514392+Max-Myagkoff@users.noreply.github.com>
2024-12-07 13:31:01 +0300

    Update test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -3,3 +3,8 @@ def test_addition():
 
 def test_subtraction():
     assert 2 - 1 == 1
+    
+def test_bubble_sort():
+    numbers = [64, 34, 25, 12, 22, 11, 90]
+    sorted_numbers = bubble_sort(numbers)
+    assert sorted_numbers == [90,64, 34, 25, 22, 12, 11]
