1ff20ec876aba20953b0226b85a6c2f793ee50ef
Max-Myagkoff <113514392+Max-Myagkoff@users.noreply.github.com>
2024-12-07 14:16:31 +0300

    Update new_test.py

--- tests/new_test.py
+++ tests/new_test.py
@@ -2,5 +2,5 @@ from bubble_sort_to_test import bubble_sort
 def test_bubble_sort():
     numbers = [64, 34, 25, 12, 22, 11, 90]
     sorted_numbers = bubble_sort(numbers)
-    answer = [90,64, 34, 25, 22, 12, 11]
+    answer = [11,12,22,25,34,64,90]
     assert sorted_numbers == answer
